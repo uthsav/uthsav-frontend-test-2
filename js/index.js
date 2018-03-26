19a4d4293e2c6011ed19b67ab44d291627d5187a
Chethan <crajendra@qwinix.io>
2018-03-26 08:41:50 +0530

    added lines

--- js/index.js
+++ js/index.js
@@ -4,12 +4,39 @@ $("#about").click(function(){
   $("#ABOUT").show();
   $("#HOME").hide();
 });
+
 $("#home").click(function(){
   $("#HOME").show();
   $("#ABOUT").hide();
 });
+
 $("#abt").click(function(){
   $("#ABOUT").show();
   $("#HOME").hide();
 });
+$("#a").click(function(){
+	
+	$("#HOME").hide();
+	$("#ABOUT").show();
 });
+$("#b").click(function(){
+	$("#ABOUT").show();
+	$("#HOME").hide();
+});
+$("#c").click(function(){
+	$("#ABOUT").show();
+	$("#HOME").hide();
+});
+});
+
+$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
